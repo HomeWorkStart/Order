6e6b0384d34e4f5c19f369fc0e13a99c951f8b51
Maria <30932255+Mashamares@users.noreply.github.com>
2017-08-28 17:27:23 +0300

    corrected shop window

--- PROGS/Shop/src/OrderWindow.java
+++ PROGS/Shop/src/OrderWindow.java
@@ -1,28 +1,33 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
+import java.lang.*;
 
-public class OrderWindow {
-
+public class OrderWindow implements ActionListener{
+//creating window components
 	JButton button_order=new JButton("–азместить заказ");
 	JLabel namemodel = new JLabel("¬ведите модель");
 	JTextField model = new JTextField();
 	JLabel number_count = new JLabel("¬ведите количество велосипедов");
 	JTextField quantity = new JTextField();
 	JFrame mainframe = new JFrame("«аказ велосипедов");
-	JComboBox spisok;
-	String[] bikes = {"turbo","nitro","duper","china"}; 
+	String[] bikes = {" ","type1","type2","type3","type4"};
+	JComboBox list = new JComboBox(bikes);
 	JPanel p1 = new JPanel();
+//////////////////////////////////////////////////////////////////////////////	
 	//  онструктор
 	OrderWindow () {
-		
+//////////////////////////////////////////////////////////////////////////////		
 		JPanel p1 = new JPanel();
 		GridLayout gl =new GridLayout(1,1);
 		p1.setLayout(gl);
 		p1.add(button_order);
 		p1.add(namemodel);
-		p1.add(model);
+		p1.add(list);
+		//p1.add(model);
 		p1.add(number_count);
 		p1.add(quantity);
+		p1.add(list);
 		
 		mainframe.setContentPane(p1);
 		//”станавливаем размер окна, так чтобы уместились
@@ -30,11 +35,36 @@ public class OrderWindow {
 		mainframe.pack();
 		//ѕоказываем окно
 		mainframe.setVisible(true);
-
+		
+		list.setSelectedIndex(5);
+		list.addActionListener(this);
 		}
-	public static void main(String[] args) {
+/*
+	void actionPerformed(ActionEvent e){
+		try{
+		bikeOrder.checkOrder("FireBird", quantity);
+		//следующа€ строка не выполн€етс€ в случае исключени€
+		txtFieldOrderConfirmation.setText(
+		"–азмещение вашего заказа завершено");
+		} catch(TooManyBikesException e){
+		txtFieldOrderConfirmation.setText(e.getMessage());
+		}
+		}
+		void checkOrder(String bikeModel, int quantity)
+		throws TooManyBikesException{
+			//Ќапишите код, который провер€ет, помещаетс€ ли требуемое
+			//количество велосипедов заданной модели в грузовик.
+			//≈сли не помещаетс€, сделать следующее:
+			throw new TooManyBikesException("Ќевозможно доставить"
+			+ quantity + " велосипедов модели " + bikeModel +
+			" за одну доставку" );
+			}
+*/			
+		public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		OrderWindow ord = new OrderWindow();
 	}
 
+	
 }
+
--- PROGS/Shop/src/OrderWindowEngine.java
+++ PROGS/Shop/src/OrderWindowEngine.java
@@ -1,6 +1,46 @@
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 
-public class OrderWindowEngine implements ActionListener {
+public class OrderWindowEngine extends OrderWindow implements ActionListener {
 	OrderWindow parent;
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		list.setSelectedIndex(4);
+		list.addActionListener((ActionListener) this);
+	}
+
 }
+//creating own exception 
+class TooManyBikesException extends Exception{
+	// Конструктор
+	TooManyBikesException (){
+	// Просто вызовите конструктор суперкласса
+	// и передайте ему сообщение, которое нужно отобразить
+	super(
+	"Мы не сможем доставить столько велосипедов за один раз.");
+	}
+	}
+
+void actionPerformed(ActionEvent e){
+	try{
+	bikeOrder.checkOrder("FireBird", quantity);
+	//следующая строка не выполняется в случае исключения
+	txtFieldOrderConfirmation.setText(
+	"Размещение вашего заказа завершено");
+	} catch(TooManyBikesException e){
+	txtFieldOrderConfirmation.setText(e.getMessage());
+	}
+	}
+	void checkOrder(String bikeModel, int quantity)
+	throws TooManyBikesException{
+		//Напишите код, который проверяет, помещается ли требуемое
+		//количество велосипедов заданной модели в грузовик.
+		//Если не помещается, сделать следующее:
+		throw new TooManyBikesException("Невозможно доставить"
+		+ quantity + " велосипедов модели " + bikeModel +
+		" за одну доставку" );
+		}
+		}
