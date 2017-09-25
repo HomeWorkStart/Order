5a447b30a1017967e8cde0099e8b41f49289a86c
Maria <30932255+Mashamares@users.noreply.github.com>
2017-09-25 16:52:40 +0300

    3 working windows: less 10, greater than 10 and exceptions with reading

--- PROGS/Shop/src/OrderWindowEngine.java
+++ PROGS/Shop/src/OrderWindowEngine.java
@@ -1,6 +1,7 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.text.JTextComponent;
 
 public class OrderWindowEngine implements ActionListener {
 	OrderWindow parent;
@@ -13,42 +14,40 @@ public class OrderWindowEngine implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		String text_quantity;
 		int int_quantity;
-		
-		text_quantity=parent.quantity.getText();
-        
-            if (text_quantity!=""){
-                try {
-                	int_quantity = Integer.parseInt(text_quantity);
-                	javax.swing.JOptionPane.showConfirmDialog(null, "You ordered", "Order",
-    						javax.swing.JOptionPane.PLAIN_MESSAGE);	
-                } catch (NumberFormatException e1) {
-              //Will Throw exception!
-              //do something! anything to handle the exception.
-                	javax.swing.JOptionPane.showConfirmDialog(null, "Пожалуйста, введите число", "Неправильный ввод",
-    						javax.swing.JOptionPane.PLAIN_MESSAGE);	                            }
-            if  (parent.int_quantity>10){
-            	javax.swing.JOptionPane.showConfirmDialog(null, "You ordered too much", "Incorrect input",
-						javax.swing.JOptionPane.PLAIN_MESSAGE);	
-         
-            	}
-            }
-            
-	}
-		//// exceptions////
-	}
-/*
-	void checkOrder(String model, double displayValue) {
+		String selectedModel = parent.list.getName();
 
-		if ((!"".equals(model)) & (displayValue != 0)) {
-			javax.swing.JOptionPane.showConfirmDialog(null, "Ваш заказ сформирован", "Статус заказа",
-					javax.swing.JOptionPane.PLAIN_MESSAGE);
-		} else if ("".equals(model)) {
-			javax.swing.JOptionPane.showConfirmDialog(null, "Ваш заказ Не сформирован. Выберите модель",
-					"Статус заказа", javax.swing.JOptionPane.PLAIN_MESSAGE);
-		} else if (displayValue == 0) {
-			javax.swing.JOptionPane.showConfirmDialog(null, "Ваш заказ Не сформирован. Выберите количество",
-					"Статус заказа", javax.swing.JOptionPane.PLAIN_MESSAGE);
+		text_quantity = parent.quantity.getText();
+
+		if (text_quantity != "") {
+			try {
+				int_quantity = Integer.parseInt(text_quantity);
+				checkOrder(selectedModel, int_quantity);
+
+			} catch (NumberFormatException e1) {
+				// Will Throw exception!
+				// do something! anything to handle the exception.
+				javax.swing.JOptionPane.showConfirmDialog(null, "Пожалуйста, введите число", "Неправильный ввод",
+						javax.swing.JOptionPane.PLAIN_MESSAGE);
+				return;
+			}
+
 		}
-	} */
 
+	}
+
+	void checkOrder(String selectedModel, int int_quantity) {
+		if (int_quantity > 10) {
+			javax.swing.JOptionPane.showConfirmDialog(null, "You ordered too much", "Incorrect input",
+					javax.swing.JOptionPane.PLAIN_MESSAGE);
+
+		}
+		
+		if ((int_quantity > 0)&&(int_quantity < 10)) {
+			javax.swing.JOptionPane.showConfirmDialog(null, "Thanks for the order" + ": " + int_quantity + " bikes of " 
+					+ selectedModel + " Model", "Shop", javax.swing.JOptionPane.PLAIN_MESSAGE);
+
+		}
+
+	}
+}
 
